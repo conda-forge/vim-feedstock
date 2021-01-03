07be39fc8d7f038bffbff63b4cb5e22e1bd51234
Kozo Nishida <kozo.nishida@gmail.com>
2021-01-04 05:38:11 +0900

    Add PYTHON3 option to nmake

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,3 +1,3 @@
 cd %SRC_DIR%\\src
-nmake -f Make_mvc.mak GUI=yes IME=yes MBYTE=yes ICONV=yes DEBUG=no
+nmake -f Make_mvc.mak GUI=yes IME=yes MBYTE=yes ICONV=yes DEBUG=no PYTHON3=C:\Miniconda
 if errorlevel 1 exit 1
