7c129e7d50574bdbaffcbe8df9f60e1b92e8578c
Kozo Nishida <kozo.nishida@gmail.com>
2021-01-03 21:35:01 +0900

    Update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,2 +1,2 @@
-nmake -f Make_mvc.mak GUI=yes IME=yes MBYTE=yes ICONV=yes DEBUG=no %SRC_DIR%\\src
+nmake -f %SRC_DIR%\\src\\Make_mvc.mak GUI=yes IME=yes MBYTE=yes ICONV=yes DEBUG=no 
 if errorlevel 1 exit 1
