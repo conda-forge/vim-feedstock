a909a7a3318436780b7942bb7865d8f3244c2735
Bradley Dice <bdice@bradleydice.com>
2021-05-29 04:55:42 -0500

    Fix cp to copy, do not build GUI.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,6 +1,5 @@
 cd %SRC_DIR%\\src
-nmake -f Make_mvc.mak GUI=no
-nmake -f Make_mvc.mak GUI=yes
-cp vim.exe %LIBRARY_BIN%
-cp gvim.exe %LIBRARY_BIN%
+nmake -f Make_mvc.mak
+if errorlevel 1 exit 1
+copy vim.exe %LIBRARY_BIN%
 if errorlevel 1 exit 1
