20b0500d04fab0c5875631d6d4e093036beb46cf
Matthias Diener <mdiener@illinois.edu>
2021-09-28 11:27:36 -0500

    more features, add test

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,6 +1,21 @@
+echo on
+
+:: Build
+
 cd %SRC_DIR%\\src
-nmake -f Make_mvc.mak
+nmake -f Make_mvc.mak ^
+		GUI=no OLE=no DIRECTX=no ^
+		FEATURES=HUGE IME=yes MBYTE=yes ICONV=yes DEBUG=no ^
+		TERMINAL=yes ^
 if errorlevel 1 exit 1
+
+
+:: Test
+
+cd %SRC_DIR%\\src\\testdir
+nmake -f Make_dos.mak VIMPROG=..\vim || exit 1
+
+:: Install
 copy vim.exe %LIBRARY_BIN%
 if errorlevel 1 exit 1
 
