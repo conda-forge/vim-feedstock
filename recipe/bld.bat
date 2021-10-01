45704cd434545f5d7f02beeeb30dc49ee4fa2a1b
Matthias Diener <mdiener@illinois.edu>
2021-10-01 14:27:26 -0500

    fix

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -6,7 +6,7 @@ set INCLUDE=%PREFIX%\\include;%INCLUDE%
 :: Unset %PYTHON% so that the build does not think we want Python2 support
 set PYTHON=
 
-set PYTHON3_VER=%PYTHON3_VER
+set PYTHON3_VER=%PYTHON3_VER%
 
 set
 
