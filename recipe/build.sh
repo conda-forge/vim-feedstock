1d24d0c012b031dfcfbdf4fca7e535744ac04d91
Matthias Diener <mdiener@illinois.edu>
2021-10-01 11:50:13 -0500

    Merge

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+set -ex
+
 # For some reason vim doesn't use standard CFLAGS for OSDEF
 # https://github.com/vim/vim/blob/5fd0f5052f9a312bb4cfe7b4176b1211d45127ee/src/Makefile#L1478
 export EXTRA_IPATHS="-I$PREFIX/include"
@@ -8,6 +12,18 @@ else
   PYTHONINTERP="--enable-pythoninterp=yes --enable-python3interp=no"
 fi
 
+if [[ "$CONDA_BUILD_CROSS_COMPILATION" == "1" && "${target_platform}" == "osx-arm64" ]]; then
+  # Need to set these explicitly for osx-arm64 since they can't be checked
+  # automatically when cross-compiling.
+  export vim_cv_toupper_broken=no
+  export vim_cv_terminfo=yes
+  export vim_cv_tgetent=non-zero
+  export vim_cv_getcwd_broken=no
+  export vim_cv_stat_ignores_slash=no
+  export vim_cv_memmove_handles_overlap=yes
+  export TERM_LIB='--with-tlib=ncurses -ltinfo'
+fi
+
 
 ./configure --prefix=$PREFIX    \
             --without-x         \
@@ -16,7 +32,8 @@ fi
             --without-local-dir \
             --enable-gui=no     \
             --enable-cscope     \
-            $PYTHONINTERP  || { cat src/auto/config.log; exit 1; }
+            $PYTHONINTERP       \
+            "$TERM_LIB" || { cat src/auto/config.log; exit 1; }
 
 make -j$CPU_COUNT
 make install
