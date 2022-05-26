be4295f5816b9b3376b4b21d31f4ddf8cf84d58d
Matthias Diener <mdiener@illinois.edu>
2022-05-26 15:54:44 -0500

    remove perl/gettext

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -20,8 +20,7 @@ nmake -f Make_mvc.mak ^
 		GUI=no OLE=no DIRECTX=no ^
 		FEATURES=HUGE IME=yes MBYTE=yes ICONV=yes DEBUG=no ^
 		TERMINAL=yes ^
-		DYNAMIC_PYTHON3=yes PYTHON3=%PREFIX% ^
-		DYNAMIC_PERL=yes PERL=%PREFIX% ^
+		DYNAMIC_PYTHON3=yes PYTHON3=%PREFIX%
 
 if errorlevel 1 exit 1
 
